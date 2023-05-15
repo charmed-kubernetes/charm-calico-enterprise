e6b10327ef2478b50efe97edcfcbc1b17c0008dd
Pedro Guimaraes <pedro.guimaraes@canonical.com>
2023-05-16 00:34:49 +0200

    Added remaining missing configuration

--- src/charm.py
+++ src/charm.py
@@ -15,7 +15,13 @@ https://discourse.charmhub.io/t/4208
 import os
 import logging
 import traceback
+import time
+import ipaddress
+import socket
 from subprocess import CalledProcessError, check_output
+from jinja2 import Environment, FileSystemLoader
+from base64 import b64decode
+
 
 from conctl import getContainerRuntimeCtl
 
@@ -35,7 +41,9 @@ TIGERA_DISTRO_VERSIONS ={
 }
 
 PLUGINS_PATH = "/usr/local/bin"
-TMP_RENDER_PATH = "/tmp/templates/rendered"
+
+
+log = logging.getLogger(__name__)
 
 
 class TigeraCalicoError(Exception):
@@ -58,9 +66,11 @@ class TigeraCalicoError(Exception):
 
 class TigeraCharm(CharmBase):
     """Charm the Tigera Calico EE."""
+    stored = StoredState()
 
     def __init__(self, *args):
         super().__init__(*args)
+        self.jinja2_environment = Environment(loader=FileSystemLoader("templates/"))
         self.framework.observe(self.on.install, self.on_install)
         self.framework.observe(self.on.config_changed, self.on_config_changed)
         self.framework.observe(self.on.remove, self.on_remove)
@@ -68,8 +78,12 @@ class TigeraCharm(CharmBase):
         self.framework.observe(self.on.upgrade_charm, self.on_upgrade_charm)
         self.framework.observe(self.on.cni_relation_joined, self.on_cni_relation_joined)
         self.framework.observe(
-            self.on.tigera_relation_changed, self.on_tigera_relation_changed
+            self.on.tigera_relation_changed, self.on_config_changed
         )
+
+        self.stored.set_default(tigera_configured=False)
+        self.stored.set_default(pod_restart_needed=False)
+
         try:
             self.CTL = getContainerRuntimeCtl()
         except RuntimeError:
@@ -91,11 +105,9 @@ class TigeraCharm(CharmBase):
                     return True
         return False
 
-    def get_registry(self):
-        registry = self.model.config["image-registry"]
-        if not registry:
-            registry = self.kube_ovn_peer_data("image-registry")
-        return registry
+    @property
+    def registry(self):
+        return self.model.config["image_registry"]
 
     def configure_cni_relation(self):
         self.unit.status = MaintenanceStatus("Configuring CNI relation")
@@ -113,12 +125,65 @@ class TigeraCharm(CharmBase):
         template.stream(**kwargs).dump(destination)
         return destination
 
+    def tigera_peer_data(self, key):
+        """Return the agreed data associated with the key
+        from each tigera unit including self.
+        If there isn't unity in the relation, return None
+        """
+        joined_data = set()
+        for relation in self.model.relations["tigera"]:
+            for unit in relation.units | {self.unit}:
+                data = relation.data[unit].get(key)
+                joined_data.add(data)
+        filtered = set(filter(bool, joined_data))
+        return filtered.pop() if len(filtered) == 1 else None
+
+    def get_ip_range(self, n):
+        return str(n).split("/")[1]
 
     #######################
     ### Tigera  Methods ###
     #######################
 
     # Following: https://docs.tigera.io/calico-enterprise/latest/getting-started/install-on-clusters/kubernetes/generic-install
+
+    def preflight_checks(self):
+        """Series of checks that should be done before any configuration change.
+        Returns True if successful on all checks pass, False otherwise.
+        """
+
+        if not self.model.config["license"]:
+            self.unit.status = BlockedStatus("Missing license config")
+            return False
+
+        try:
+            pod_cidr = ipaddress.ip_network(self.model.config["pod_cidr"])
+        except ValueError as e:
+            self.unit.status = BlockedStatus("Pod-to-Pod configuration is not valid CIDR")
+            return False
+        try:
+            stable_ip_cidr = ipaddress.ip_network(self.model.config["stable_ip_cidr"])
+        except ValueError as e:
+            self.unit.status = BlockedStatus("Stable IP network is not valid CIDR")
+            return False
+
+        hostname_found = False
+        for hostname,p in self.model.config["bgp_parameters"].items():
+            s = p["stableAddress"]
+            if ipaddress.ip_address(s) not in stable_ip_cidr:
+                self.unit.status = BlockedStatus(f"{s} is not present in {stable_ip_cidr}")
+                return False
+            if hostname == socket.get_hostname():
+                hostname_found = True
+        if not hostname_found:
+            self.unit.status = BlockedStatus("This node has no entry in the bgp_parameters")
+            return False
+
+        if self.model.config["addons"] and not self.model.config["addons_storage_class"]:
+            self.unit.status = BlockedStatus("Addons specified but missing addons_storage_class info")
+            return False
+
+        return True
 
     @property
     def kubernetes_version(self):
@@ -148,6 +213,7 @@ class TigeraCharm(CharmBase):
              """'jsonpath={..status.conditions[?(@.type=="Ready")].status}'""")
         return True if "True" in output else False
 
+    """
     def pull_cnx_node_image(self):
         image = self.model.resources.fetch('cnx-node-image')
 
@@ -163,6 +229,7 @@ class TigeraCharm(CharmBase):
                 with open(unzipped, 'wb') as f_out:
                     f_out.write(f_in.read())
             CTL.load(unzipped)
+    """
 
     def check_tigera_status(self):
         """Checks if  tigera operator is ready.
@@ -175,12 +242,31 @@ class TigeraCharm(CharmBase):
         return True if "True" in output else False
 
     def implement_early_network(self):
+        """ Implement the Early Network
+
+        The following steps must be executed:
+        1) early network: installs the cnx-node container and configures the BGP tunnel
+        2) deploy cnx-node systemd
+        3) wait until cnx-node is listening on port 8179
+        """
+
         ## TODO: implement the early_network
         ## ...
         os.makedirs("/calico-early/", exist_ok=True)
         self.render_template(
             "templates/calico_bgp_layout.yaml.j2", "/calico-early/cfg.yaml", **self.config["bgp_parameters"])
 
+
+    def waiting_for_cni_relation(self):
+        service_cidr = self.tigera_peer_data("service-cidr")
+        registry = self.get_registry()
+        return not self.is_kubeconfig_available() or not service_cidr or not registry
+
+    def configure_bgp(self):
+        bgp_parameters = self.model.config["bgp_parameters"]
+        if not bgp_parameters:
+            return
+        
 
     ###########################
     ### Charm Event Methods ###
@@ -189,22 +275,125 @@ class TigeraCharm(CharmBase):
     def on_install(self, event):
         """Installation routine.
 
-        The following steps must be executed:
-        1) early network: installs the cnx-node container and configures the BGP tunnel
-        2) deploy cnx-node systemd
-        3) wait until cnx-node is listening on port 8179
+        Execute the Early Network stage if requested. The config of Tigera itself depends on k8s deployed
+        and should be executed in the config-changed hook.
         """
-        if self.config(self.config("disable_early_network")):
-            return
-        self.implement_early_network()
+        if not self.config(self.config("disable_early_network")):
+            self.implement_early_network()
+        
 
-    def on_config_changed(self, event):
-        """Config changed event processing
+    def on_update_status(self, event):
+        """State machine of the charm:
+
+        1) Only change the status if in active
+        2) If kubeconfig and CNI relation exist
         """
-        if not self.is_kubeconfig_available() or not service_cidr or not registry:
+
+        if isinstance(self.unit.status, ActiveStatus):
+            log.info("on_update_status: unit not in active status: {self.unit.status}")
+        if self.waiting_for_cni_relation():
             self.unit.status = WaitingStatus("Waiting for CNI relation")
             return
 
+    def on_cni_relation_joined(self, event):
+        self.configure_cni_relation()
+        self.set_active_status()
+
+
+    def on_config_changed(self, event):
+        """Config changed event processing
+
+        The leader needs to know the BGP information about every node and only the leader should apply
+        the changes in the deployment.
+        1) Check if the CNI relation exists
+        2) Return if not leader
+        3) Apply tigera operator
+        """
+        self.stored.tigera_configured = False
+        if self.preflight_checks():
+            return
+        service_cidr = self.tigera_peer_data("service-cidr")
+
+        if self.waiting_for_cni_relation():
+            self.unit.status = WaitingStatus("Waiting for CNI relation")
+            return
+
+        if not self.unit.is_leader():
+            # Only the leader should manage the operator setup
+            log.info("on_config_changed: detected k8s is up but this unit is not the leader, leaving...")
+            self.unit.status = ActiveStatus("Node Configured")
+            self.stored.tigera_configured = True
+            return
+
+        self.unit.status = MaintenanceStatus("Applying Tigera Operator")
+        self.apply_tigera_operator()
+        for i in range(0,10):
+            self.unit.status = MaintenanceStatus(f"Wait #{i} for the tigera operator...")
+            if self.check_tigera_status():
+                break
+            time.sleep(24)
+        self.unit.status = ActiveStatus("Finished deploying Tigera Operator")
+
+        self.unit.status = MaintenanceStatus("Configuring image secret and license file...")
+        if self.model.config["image_registry"] and self.model.config["image_registry_secret"]:
+            image_secret = '\'{"auths":{"' + self.model.config["image_registry"] + '":{"auth":"' + self.model.config["image_registry_secret"] + '","email":""}}}\''
+            self.kubectl(
+                "apply","secret", "generic", "tigera-pull-secret",
+                f"--from-literal=.dockerconfigjson={image_secret}",
+                "--type=kubernetes.io/dockerconfigjson", "-n", "tigera-operator"
+            )
+        self.kubectl("apply", "-f", b64decode(self.model.config["license"]).rstrip())
+
+        self.unit.status = MaintenanceStatus("Generating bgp yamls...")
+        self.render_template(
+            "bgp_layout.yaml.j2",
+            "/tmp/bgp_layout.yaml",
+            {"bgp_parameters": self.model.config["bgp_parameters"]})
+        self.kubectl("apply", "-f", "-", "/tmp/bgp_layout.yaml")
+        self.render_template(
+            "bgppeer.yaml.j2",
+            "/tmp/bgppeer.yaml",
+            {"bgp_parameters": self.model.config["bgp_parameters"]})
+        self.kubectl("apply", "-f", "-", "/tmp/bgppeer.yaml")
+        self.render_template(
+            "ippools.yaml.j2",
+            "/tmp/ippools.yaml",{
+                "pod_cidr_range": self.get_ip_range(self.model.config["pod_cidr"]),
+                "pod_cidr": self.model.config["pod_cidr"],
+                "stable_ip_cidr": self.model.config["stable_ip_cidr"]
+            })
+        self.kubectl("apply", "-f", "-", "/tmp/ippools.yaml")
+
+        self.unit.status = MaintenanceStatus("Applying Installation CRD")
+        self.render_template(
+            "calico_enterprise_install.yaml.j2",
+            "/tmp/calico_enterprise_install.yaml",
+            {
+                "image_registry": self.model.config["image_registry"],
+                "image_registry_secret": self.model.config["image_registry_secret"],
+                "image_path": self.model.config["image_path"],
+                "image_prefix": self.model.config["image_prefix"]
+            })
+        self.kubectl("apply", "-f", "-", "/tmp/calico_enterprise_install.yaml")
+
+        self.kubectl(
+            "patch", "bgpconfiguration.projectcalico.org", "default", "-p",
+            '\'{"spec":{"serviceClusterIPs": [{"cidr": "{{' + service_cidr + '}}"}]}}\''
+        )
+
+
+        if self.model.config["addons"]:
+            self.unit.status = MaintenanceStatus("Applying Addons")
+            self.render_template(
+                "addons.yaml.j2",
+                "/tmp/addons.yaml",
+                {
+                    "addons_storage_class": self.model.config["addons_storage_class"]
+                })
+            self.kubectl("apply", "-f", "-", "/tmp/addons.yaml")
+
+        self.unit.status = ActiveStatus("Node Configured")
+        self.stored.tigera_configured = True
 
 
 if __name__ == "__main__":  # pragma: nocover
