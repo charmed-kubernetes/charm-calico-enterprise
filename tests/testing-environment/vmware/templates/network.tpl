8da300d3057ec06a34f6ad7812fa02eb996edf09
Adam Dyess <adam.dyess@canonical.com>
2023-09-13 15:23:35 -0500

    mark TF variables as sensative

--- tests/testing-environment/vmware/templates/network.tpl
+++
@@ -1,9 +0,0 @@
-#cloud-config
-network:
-  version: 2
-  ethernets:
-      eth0:
-          dhcp4: true
-      eth1:
-          dhcp4: false
-          addresses: [${switch_network}.${switch_final_octet}/24]
--- tests/testing-environment/vmware/variables.tf
+++ tests/testing-environment/vmware/variables.tf
@@ -1,31 +1,41 @@
+variable "vsphere_server" {
+    description = "IP Address of the VSphere Environment."
+    sensitive   = true
+    type        = string
+}
+
 variable "vsphere_user" {
-    type = string
+    description = "Username for VSphere Environment"
+    type        = string
+    sensitive   = true
 }
 
 variable "vsphere_password" {
-    type = string
+    description = "Password for VSphere Environment"
+    type        = string
+    sensitive   = true
 }
 
 variable "vsphere_folder" {
+    description = "Folder location in which vsphere starts the vm instances"
     type = string
 }
 
 variable "juju_authorized_key" {
-    type = string
-}
-
-variable "vsphere_server" {
+    description = "Public key juju will use when adding the machines to the model."
     type = string
 }
 
 variable "tigera_registry_user" {
     type = string
     default = "VALUE NOT SET"
+    sensitive = true
 }
 
 variable "tigera_registry_password" {
     type = string
     default = "VALUE NOT SET"
+    sensitive = true
 }
 
 variable "calico_early_version" {
